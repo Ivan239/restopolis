9249f7ae745e0afe476aab09100aeb12945a0c53
ivan239 <ivan-obraztsov@yandex.ru>
2022-04-04 17:58:20 +0300

    tests

--- src/components/BigCard/BigCard.jsx
+++ src/components/BigCard/BigCard.jsx
@@ -43,11 +43,12 @@ const BigCard = (props) => {
 
     return <div className='big-card'>
         <div className='big-card__content' onClick={(e) => handleBigCard(e)}>
+            <img src={close} alt='close' className='big-card__close'/>
             <img className='big-card__image' src={img} alt='pizza_image' />
             <div className='big-card__info'>
                 <h1 className='big-card__name'>{name}</h1>
                 <p className='big-card__description'>Chicken pieces, bell peppers, cheddar and parmesan cheese mix, mozzarella, red onion, sweet chili sauce, alfredo sauce</p>
-                <Toppings isSelected={isSelected}/>
+                <Toppings isSelected={isSelected} />
                 <div className='big-card__button'>
                     <MenuButton onClick={() => addToCart()}>
                         <h3 className='big-card__price'>Add to cart for ${fullPrice}</h3>
@@ -55,7 +56,6 @@ const BigCard = (props) => {
                 </div>
             </div>
         </div>
-        <img src={close} alt='close' className='big-card__close' />
     </div>;
 };
 
