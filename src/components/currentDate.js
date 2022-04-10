6c1cb6564783e5b7abf0c33bdb704f96b6e81fb0
ivanchik239 <ivan-obraztsov@ya.ru>
2022-04-10 22:52:14 +0300

    1.2.1 fix eslint

--- src/components/currentDate.js
+++ src/components/currentDate.js
@@ -1,8 +1,8 @@
 export default function currentDate(addHours) {
-    let date = new Date();
-    date.setHours(date.getHours() + addHours);
-    date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-    date = date.toISOString().slice(0,16);
+  let date = new Date();
+  date.setHours(date.getHours() + addHours);
+  date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  date = date.toISOString().slice(0, 16);
 
-    return date;
+  return date;
 }
--- src/components/newId.js
+++ src/components/newId.js
@@ -1,4 +1,4 @@
 export default function newId() {
-    return Math.floor((1 + Math.random()) * 0x10000000)
-        .toString(16)
+  return Math.floor((1 + Math.random()) * 0x10000000)
+    .toString(16);
 }
